64ce871cda313bd4e3f12758e2e31c7d7a566ea9
maxbkhts0 <maxbkhts0@gmail.com>
2022-09-07 01:17:55 +1000

    worked on nav menu

---
+++ main.js
@@ -0,0 +1,12 @@
+// toggle nav menu when menu icon is clicked
+
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener('click', () => {
+  primaryNav.hasAttribute('data-visible') 
+    ? navToggle.setAttribute('aria-expanded', false)
+    : navToggle.setAttribute('aria-expanded', true);
+  primaryNav.toggleAttribute('data-visible');
+});
+
